50edc87e373d01d44ad144c4ee003242b015f73a
Gabriel Costa <gabrielcostamgm@hotmail.com>
2025-11-26 13:37:11 -0300

    FIX: CODIGO LSP DE EXEMPLO 'CORRETO' DIZIA QUEBRAR AO RECEBER METODO DE PAGAMENTO. RETIRADO COMENTARIO INCOERENTE

--- faminas-poo-solid/src/parte1EstudosCaso/lsp/ExemploCorreto/ClienteAplicacao.java
+++ faminas-poo-solid/src/parte1EstudosCaso/lsp/ExemploCorreto/ClienteAplicacao.java
@@ -3,12 +3,12 @@ package ExemploCorreto;
 public class ClienteAplicacao {
     public static void processar(PagamentoBase pagamento) {
         double total = pagamento.calcularTotal(); 
-        System.out.println("Processando " + pagamento.getClass().getSimpleName() + "...");
+        System.out.println("Processando " + 
+        pagamento.getClass().getSimpleName() + "...");
         System.out.println("Total a pagar: R$ " + total);
     }
-
     public static void main(String[] args) {
         processar(new PagamentoCartao(100.00)); // Funciona
-        processar(new PagamentoDinheiroCorreto(100.00)); // Quebra a aplicação!
+        processar(new PagamentoDinheiroCorreto(100.00));
     }
 }
